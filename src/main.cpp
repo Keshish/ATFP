c70665cd588ff09e89069f08baa1360244ed6f35
keshish <garo.keshish@gmail.com>
2023-09-29 20:43:06 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,7 @@
 
 #include "Display/Display.h"
 
-#include "opencv2/video/tracking.hpp"
-
-using namespace cv;
-
 int main() {
-    KalmanFilter KF(2, 1, 0);
-
     auto display = &Display::display();
     display->run();
     return 0;
